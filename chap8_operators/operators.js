fd55963844c13808605a4df33c570b4b52f48436
Kim Do Ky <makingfunk0@gmail.com>
2018-12-21 09:13:28 +0900

    chap8_operators

---
+++ chap8_operators/operators.js
@@ -0,0 +1,18 @@
+// string operators
+txt1 = "John";
+txt2 = "Doe";
+txt3 = txt1 + " " + txt2;
+
+// result : John Doe
+
+// +=
+txt1 = "What a very ";
+txt1 += "nice day";
+
+// result : What a very nice day
+
+// strings and numbers
+x = 5 + 5; // 10
+y = "5" + 5; // 55
+z = "Hello" + 5; // Hello5
+
